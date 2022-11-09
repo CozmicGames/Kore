d4237fa9b0500522adc2be67499468ac27c1f44c
cozmic185 <max.schott@hotmail.de>
2022-11-09 08:27:04 +0100

    Simplify UUID.kt

--- src/commonMain/kotlin/com/cozmicgames/utils/UUID.kt
+++ src/commonMain/kotlin/com/cozmicgames/utils/UUID.kt
@@ -1,47 +1,42 @@
 package com.cozmicgames.utils
 
-import com.cozmicgames.utils.extensions.format
 import kotlin.random.Random
 
-class UUID(private val data: ByteArray) {
+class UUID(data: CharArray) {
     companion object {
-        private val regex = Regex("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", RegexOption.IGNORE_CASE)
+        private val CHARS = "0123456789abcdefghijklmnopqrstuvwxyz"
+        private val SIZE = 16
+        private val REGEX = Regex("[a-z0-9]{$SIZE}")
 
-        private fun fix(data: ByteArray, version: Int, variant: Int): ByteArray {
-            data[6] = ((data[6].toInt() and 0b00001111) or (version shl 4)).toByte()
-            data[8] = ((data[8].toInt() and 0x00111111) or (variant shl 6)).toByte()
-            return data
+        fun randomUUID(random: Random = Random): UUID {
+            return UUID(CharArray(SIZE) { CHARS.random(random) })
         }
 
-        fun random(random: Random = Random): UUID = UUID(fix(random.nextBytes(16), 4, 1))
+        operator fun invoke(string: String): UUID {
+            if (REGEX.matchEntire(string.lowercase()) == null)
+                throw RuntimeException("Invalid uuid string: $string")
 
-        operator fun invoke(str: String): UUID {
-            require(regex.matchEntire(str) != null) { "Invalid UUID" }
-            return UUID(Hex.decode(str.replace("-", "")))
+            return UUID(string.lowercase().toCharArray())
         }
     }
 
-    val version: Int get() = (data[6].toInt() ushr 4) and 0b1111
+    private val data = data.joinToString(separator = "")
 
-    val variant: Int get() = (data[8].toInt() ushr 6) and 0b11
-
-    private val hashCode = data.contentHashCode()
-
-    override fun toString() = "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x".format(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15])
+    override fun toString(): String {
+        return data
+    }
 
     override fun hashCode(): Int {
-        return hashCode
+        return data.hashCode()
     }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other !is UUID) return false
+        if (other == null || this::class != other::class) return false
 
-        if (hashCode != other.hashCode)
-            return false
+        other as UUID
 
-        if (!data.contentEquals(other.data))
-            return false
+        if (data != other.data) return false
 
         return true
     }
